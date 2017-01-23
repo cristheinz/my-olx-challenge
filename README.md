5cb7c80e209f67e78ce21c16b407cfe0472f087c
Christiano <christiano.domingues@gmail.com>
2017-01-23 15:32:37 +0000

    layout w bootstrap

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ This project was generated with [angular-cli](https://github.com/angular/angular
 
 ## Development server
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+Run `ng serve --host 0.0.0.0 --port 8080` on cloud9.io
 
 ## Code scaffolding
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  items = [
+    {name: "item 1", isActive: false},
+    {name: "item 2", isActive: false},
+    {name: "item 3", isActive: false},
+    {name: "item 4", isActive: false},
+    {name: "item 5", isActive: true},
+    {name: "item 6", isActive: true}
+  ];
 }
